9657f21de2ece2071fc2048b2ab8af1eaf07c4f4
dev-whoan <dev.whoan@gmail.com>
2023-02-26 18:36:24 +0900

    feat. Health Check Updated [GET] /healthz : 200 OK

---
+++ src/health-check/health-check.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+import { Response } from 'express';
+
+@Controller('healthz')
+export class HealthCheckController {
+  @Get()
+  healthCheck(@Res() res: Response) {
+    return res.status(HttpStatus.OK).send('OK');
+  }
+}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('Health Check -> [(GET) /healthz]', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/healthz')
       .expect(200)
-      .expect('Hello World!');
+      .expect('OK');
   });
 });
