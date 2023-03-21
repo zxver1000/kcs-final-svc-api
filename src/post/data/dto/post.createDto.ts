2476b56d4f58e6af339b888c592f2b5645bee046
hyeonho ko <hyeonhoko@hyeonhoui-noteubug.local>
2023-03-21 11:28:53 +0900

    ADD APIGATEWAY POST contoroller

--- src/post/data/dto/post.createDto.ts
+++ src/post/data/dto/post.createDto.ts
@@ -1,6 +1,5 @@
 import { Outlay } from '../post.Outlay';
 import { Weather } from '../post.Weather';
-
 import { PostDate } from '../post.PostDate';
 export class PostCreateDto {
   owner: string;
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -7,9 +7,12 @@ import {
   Param,
   Patch,
   Post,
+  Query,
+  Req,
   UnauthorizedException,
   UseGuards,
 } from '@nestjs/common';
+import { Request } from 'express';
 import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 import { AuthService } from 'src/auth/service/auth.service';
 import { CurrentUser } from 'src/common/decorator/user.decorator';
@@ -25,25 +28,27 @@ export class PostController {
     private readonly authService: AuthService,
   ) {}
 
-  @Get('/:data')
+  @Get(':data')
   @UseGuards(JwtAuthGuard)
   async getPost(@CurrentUser() user, @Param('data') data: string) {
     if (!user) {
       return new UnauthorizedException('유저 정보를 확인해 주세요.');
     }
 
-    if (data.length > 20) {
-      //아이디준거
-      return this.PostService.getPersonalDiary(data, user.id);
-    } else {
-      //pagenumber!
-      let page_num = Number(data);
-      let k = await this.PostService.listDiaries(page_num, user.id);
-      console.log(k);
-
-      return await this.PostService.listDiaries(page_num, user.id);
-    }
+    return this.PostService.getPersonalDiary(data, user.id);
   }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async getPosts(@CurrentUser() user, @Req() data: Request) {
+    if (!user) {
+      return new UnauthorizedException('유저 정보를 확인해 주세요.');
+    }
+
+    let page_num = Number(data.query['pagenum']);
+    return await this.PostService.listDiaries(page_num, user.id);
+  }
+
   @Post()
   @UseGuards(JwtAuthGuard)
   async addPersonalDiary(@CurrentUser() user, @Body() data: object) {
