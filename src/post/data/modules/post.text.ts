4944f4e3efa28a611db6fac6fbac4ba4a185fdb8
Eugene <65178775+dev-whoan@users.noreply.github.com>
2023-03-22 12:35:01 +0900

    Update post.text.ts

--- src/post/data/modules/post.text.ts
+++ src/post/data/modules/post.text.ts
@@ -31,7 +31,7 @@ export class PostText {
 }
 
 export const deserializePostText = (target: object): PostText => {
-  if (!target || target == undefined) return null;
+  if (!target) return null;
 
   return new PostText(
     target['owner'],
