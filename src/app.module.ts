530b9d87d85b1b99b78b2d5bf5e8330d62c45837
Eugene <65178775+dev-whoan@users.noreply.github.com>
2023-03-22 14:38:01 +0900

    mongoose log

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthCheckModule } from './health-check/health-check.module';
 import { PostModule } from './post/post.module';
+import mongoose from 'mongoose';
 
 @Module({
   imports: [
@@ -19,4 +20,8 @@ import { PostModule } from './post/post.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    mongoose.set('debug', true);
+  }
+}
